70b3bf916d9193425e3afe8649c9ef620465bd79
Kevin Loftis <loftiskg@mail.lipscomb.edu>
2018-01-18 23:10:19 -0800

    added get_time_seconds function; typing test now times user

--- CS251/Homework1/TypingSpeed/typing-speed.c
+++ CS251/Homework1/TypingSpeed/typing-speed.c
@@ -1,9 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <sys/time.h>
 #include <time.h>
 
+/*returns the current time in seconds*/
+double
+get_time_seconds()
+{
+  struct timeval tv;
+  double currtime;
 
+  gettimeofday(&tv, NULL);
+  currtime = ((double)tv.tv_sec + (double)tv.tv_usec / 1000000);
+
+  return currtime;
+}
 
 
 /* returns a pointer to an array of shuffled integers 0-array_len-1 */
@@ -14,11 +26,12 @@ shuffle_n_integers(int array_len)
   int temp_values[array_len];
   int index_tracker[array_len];
   int i;
-  
+ 
   for(i=0;i<array_len;i++){
     temp_values[i]=i;     // temporary ordered list
     index_tracker[i] = 1; // keeps track of what indicies have already been used
   }
+
   
   int j = 0;
   for(i=0;i<array_len;i++){
@@ -59,27 +72,32 @@ int
 main(){
   srand(time(0));
   int num_words = 9;
-  const char *words[]={"The","quick","brown","fox","jumps","over","the","lazy","dog"};
+  const char *words[]={"The","quick","brown","fox",
+		       "jumps","over","the","lazy","dog"};
+  
+  double tic,toc = 0;                                      //variables used to time the user
+  int *rand_indicies = shuffle_n_integers(num_words);      // used as indicies to shuffle words
+  char user_input[6];                                      // buffer to hold user input
 
   printf("This is an application to test your typing speed.  Press enter to start\n");
   getchar();  /* Waits for user to press enter */
 
-
-  int *rand_indicies = shuffle_n_integers(num_words);
+  tic = get_time_seconds();
   int i;
-  char user_input[6];
   for(i = 0;i<num_words;i++){
     printf("Word #%d: %s ",i+1,words[rand_indicies[i]]);
-    scanf("%6s",user_input); // chose 6 b/c the words that we are matching are no longer than 6 chars.
-    flush();
-    
+    scanf("%6s",user_input);                             //char limit is 6 because no word is over 6 chars
+    flush();                                             //clear stdin
+
+    // check if user input matches word[i]
     while((strncmp(words[rand_indicies[i]], user_input, strlen(words[rand_indicies[i]]))) != 0){
       printf("Incorrect, try again.\n");
       scanf("%6s",user_input);
       flush();
     }
   }
-
+  toc = get_time_seconds();
+  printf("It took you %lf seconds!\n", toc-tic);
   return 0;
 }
 
